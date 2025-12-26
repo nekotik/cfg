eca345f130d9f0098a0ad56e67efc951c6bf6178
nekotik <olegkotyuk1@gmail.com>
2025-12-27 01:47:09 +0200

    other

---
+++ nvim/init.lua
@@ -0,0 +1,15 @@
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.cursorline = true
+vim.o.signcolumn = "yes"
+vim.o.wrap = false
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.smartindent = true
+vim.o.autoindent = true
+vim.o.swapfile = false
+vim.g.mapleader = " "
+vim.o.winborder = "rounded"
+vim.o.termguicolors = true
+vim.o.clipboard = "unnamedplus"
